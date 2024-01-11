fb0b670b4bb08c759da34562075045267bcfe279
BarstanFilip <145748595+BarstanFilip@users.noreply.github.com>
2024-01-11 14:04:07 +0200

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -1,86 +1,56 @@
-#include <iostream>
+﻿#include <iostream>
+#include <fstream>
+#include <set>
+#include <string>
+#include <algorithm>
 using namespace std;
-template <typename T>
-class Node {
-public:
-    T data;
-    Node* next;
-
-    Node(T val) : data(val), next(nullptr) {}
-};
-
-template <typename T>
-class LinkedList {
-private:
-    Node<T>* head;
-
-public:
-
-    LinkedList() : head(nullptr) {}
-
-    LinkedList(const LinkedList& other) {
-      
-    }
-
-
-    ~LinkedList() {
-
-    }
-
-    LinkedList& operator=(const LinkedList& other) {
-
-        return *this;
-    }
-
- 
-    bool operator==(const LinkedList& other) const {
-        
-        return false; 
-    }
-
-    template <typename U>
-    friend ostream& operator<<(ostream& os, const LinkedList<T>& list);
-
- 
-    template <typename U>
-    friend istream& operator>>(istream& is, LinkedList<U>& list);
-
-    void insert(const T& value) {
-        
-    }
-
-    
-    void remove(const T& value) {
-
-    }
-};
-
-
-template <typename T>
-ostream& operator<<(ostream& os, const LinkedList<T>& list) {
- 
-    return os;
-}
-
-
-template <typename T>
-istream& operator>>(istream& is, LinkedList<T>& list) {
-   
-    return is;
-}
-
 int main() {
-    
-    LinkedList<int> myList;
-    myList.insert(1);
-    myList.insert(2);
-    myList.insert(3);
 
- cout << "Lista initiala: " << myList << endl;
+    ifstream inputFile("input.txt");
+    if (!inputFile.is_open()) {
+        cerr << "Nu s-a putut deschide fisierul input.txt.\n";
+        return 1;
+    }
 
-    myList.remove(2);
+   
+    set<string> cvUnice;
 
-   cout << "Lista dupa stergere: " << myList << endl;
+  string cuvant;
+    while (inputFile >> cuvant) {
+        
+        cuvant.erase(remove_if(cuvant.begin(), cuvant.end(), [](char c) {
+            return !isalpha(c);
+            }), cuvant.end());
+
+   
+        if (cuvant.empty()) {
+            continue;
+        }
+
+       
+        transform(cuvant.begin(), cuvant.end(), cuvant.begin(), ::tolower);
+
+        cvUnice.insert(cuvant);
+    }
+
+
+    inputFile.close();
+
+    ofstream outputFile("output.txt");
+    if (!outputFile.is_open()) {
+       cerr << "nu se poate deschide fisierul.\n";
+        return 1;
+    }
+
+  
+    for (const auto& cUnic : cvUnice) {
+        outputFile << cUnic << "\n";
+    }
+
+   
+    outputFile.close();
+
+   cout << "Verificati output.\n";
 
     return 0;
 }
